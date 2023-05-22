53115cc9bfd874f52b9f98d992a0225126180781
Anton Zakharov <hello@antonzakharov.ru>
2023-05-22 14:23:27 +0300

    first commit

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"context"
-	"crypto/tls"
+	"database/sql"
 	"fmt"
-	"log"
-
 	"github.com/ClickHouse/clickhouse-go/v2"
-	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"log"
 )
 
 const (
-	configFile              = "config.yaml"
-	clickhouseClientName    = "an-example-go-client"
-	clickhouseClientVersion = "0.1"
+	configFile = "config.yaml"
+	//clickhouseClientName    = "an-example-go-client"
+	//clickhouseClientVersion = "0.1"
 )
 
 func main() {
@@ -30,64 +28,93 @@ func main() {
 	}
 
 	ctx := context.Background()
-	rows, err := conn.Query(ctx, config.App.Query)
+	rows, err := conn.QueryContext(ctx, config.App.Query)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for rows.Next() {
 		var (
-			name, uuid string
+			count uint64
 		)
-		if err := rows.Scan(
-			&name,
-			&uuid,
-		); err != nil {
+		if err := rows.Scan(&count); err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("name: %s, uuid: %s",
-			name, uuid)
+		fmt.Printf("%d", count)
 	}
 
 }
 
-func connect(c *Configuration) (driver.Conn, error) {
-	var (
-		ctx       = context.Background()
-		conn, err = clickhouse.Open(&clickhouse.Options{
-			Addr: []string{c.Database.Addr},
-			Auth: clickhouse.Auth{
-				Database: c.Database.Database,
-				Username: c.Database.Username,
-				Password: c.Database.Password,
-			},
-			ClientInfo: clickhouse.ClientInfo{
-				Products: []struct {
-					Name    string
-					Version string
-				}{
-					{Name: clickhouseClientName, Version: clickhouseClientVersion},
-				},
-			},
+func connect(c *Configuration) (*sql.DB, error) {
+	//var (
+	//	ctx  = context.Background()
+	//	conn = clickhouse.OpenDB(&clickhouse.Options{
+	//		Addr: []string{c.Database.Addr},
+	//		Auth: clickhouse.Auth{
+	//			Database: c.Database.Database,
+	//			Username: c.Database.Username,
+	//			Password: c.Database.Password,
+	//		},
+	//		TLS: nil,
+	//		Settings: clickhouse.Settings{
+	//			"max_execution_time": 60,
+	//		},
+	//		DialTimeout:          time.Second * 30,
+	//		Debug:                false,
+	//		BlockBufferSize:      10,
+	//		MaxCompressionBuffer: 10240,
+	//		ClientInfo: clickhouse.ClientInfo{
+	//			Products: []struct {
+	//				Name    string
+	//				Version string
+	//			}{
+	//				{Name: clickhouseClientName, Version: clickhouseClientVersion},
+	//			},
+	//		},
+	//	})
+	//conn, err = clickhouse.Open(&clickhouse.Options{
+	//	Addr: []string{c.Database.Addr},
+	//	Auth: clickhouse.Auth{
+	//		Database: c.Database.Database,
+	//		Username: c.Database.Username,
+	//		Password: c.Database.Password,
+	//	},
+	//	ClientInfo: clickhouse.ClientInfo{
+	//		Products: []struct {
+	//			Name    string
+	//			Version string
+	//		}{
+	//			{Name: clickhouseClientName, Version: clickhouseClientVersion},
+	//		},
+	//	},
+	//	Debug: false,
+	//	Debugf: func(format string, v ...interface{}) {
+	//		fmt.Printf(format, v)
+	//	},
+	//	TLS: nil,
+	//})
+	//)
 
-			Debugf: func(format string, v ...interface{}) {
-				fmt.Printf(format, v)
-			},
-			TLS: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		})
-	)
-
+	connStr := fmt.Sprintf("tcp://%s/%s?dial_timeout=1s%s", c.Database.Addr, c.Database.Database, getCreds(*c))
+	conn, err := sql.Open("clickhouse", connStr)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := conn.Ping(ctx); err != nil {
+	if err = conn.PingContext(context.Background()); err != nil {
 		if exception, ok := err.(*clickhouse.Exception); ok {
 			fmt.Printf("Exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
 		}
 		return nil, err
 	}
 	return conn, nil
+}
+
+func getCreds(cfg Configuration) (creds string) {
+
+	if cfg.Database.Username != "" {
+		creds = fmt.Sprintf("&username=%s&password=%s", cfg.Database.Username, cfg.Database.Password)
+	}
+
+	return creds
 }
